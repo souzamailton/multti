955396486fd8d34f4189d7fa9a80e1b4bb958054
Mailton Souza <mailtonjunior@gmail.com>
2025-12-30 15:27:29 -0500

    Switch to PostgreSQL for Render deployment

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from config import Config
+import os
 
 db = SQLAlchemy()
 login_manager = LoginManager()
@@ -9,7 +10,9 @@ login_manager.login_view = 'main.login'
 
 def create_app():
     app = Flask(__name__)
-    app.config.from_object(Config)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
 
     db.init_app(app)
     login_manager.init_app(app)
